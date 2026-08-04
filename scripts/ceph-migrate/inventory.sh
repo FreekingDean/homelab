ad65642bd37eb0def1cb6ce67748df5e4b3c5f4c
Dean Galvin <deangalvin3@gmail.com>
2026-08-03 23:43:22 -0400

    create storage

--- scripts/ceph-migrate/inventory.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-kubectl get pv -o json | jq -r '
-  .items[]
-  | select(.spec.csi.driver | startswith("rook-ceph."))
-  | [.spec.claimRef.namespace, .spec.claimRef.name, .spec.storageClassName, .spec.capacity.storage] | @tsv
-' | sort | column -t
